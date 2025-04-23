0a1deb8ec25aa51a521f5463bd41a461187a1285
tchindavaldo <tchindavaldoblair@gmail.com>
2025-04-23 22:10:02 +0100

    some update 2

--- src/socket.js
+++ src/socket.js
@@ -1,44 +1,35 @@
 // src/socket.js
 let io;
 
-module.exports = 
+module.exports = {
+  init: server => {
+    io = require('socket.io')(server, {
+      cors: {
+        origin: '*',
+        methods: ['*'],
+        allowedHeaders: ['*'],
+        credentials: true,
+      },
+    });
 
-{
-    init: (server) => 
-{
-    io = require("socket.io")(server, 
-{
-    cors: 
-{
-    origin: "*", methods: ["*"], allowedHeaders: ["*"], credentials: true, }, });
+    io.on('connection', socket => {
+      console.log('🟢 Client connecté :', socket.id);
+      socket.on('join_fastfood', fastfoodId => {
+        socket.join(fastfoodId);
+        console.log(`🔐 Socket ${socket.id} a rejoint la room fastfood: ${fastfoodId}`);
+      });
+      socket.on('disconnect', () => {
+        console.log('🔴 Client déconnecté :', socket.id);
+      });
+    });
 
-        io.on("connection", (socket) => 
-{
-    console.log("🟢 Client connecté :", socket.id);
-            socket.on("join_fastfood", (fastfoodId) => 
-{
-    socket.join(fastfoodId); console.log(`🔐 Socket $
-{
-    socket.id} a rejoint la room fastfood: $
-{
-    fastfoodId}`); });
-            socket.on("disconnect", () => 
-{
-    console.log("🔴 Client déconnecté :", socket.id); });
+    return io;
+  },
 
-        });
-
-        return io;
-
-    },
-
-    getIO: () => 
-{
-    if (!io) 
-
-{
-    throw new Error("Socket.io non initialisé !"); }
-        return io;
-
+  getIO: () => {
+    if (!io) {
+      throw new Error('Socket.io non initialisé !');
     }
+    return io;
+  },
 };
